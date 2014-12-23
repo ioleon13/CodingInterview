66c09e88dd0b50e78c1f859ec7cb97244fc2e881
ioleon13 <liya.lhi@gmail.com>
2014-12-23 23:37:48 +0800

    A new solution:O(n) — Moore voting algorithm
    The old solution was a hash table, Space was O(n), the new solution's Space was O(1)

--- LeetCode/src/MajorityElement/Solution1.cpp
+++ LeetCode/src/MajorityElement/Solution1.cpp
@@ -8,10 +8,10 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
-#include <map>
+//#include <map>
 using namespace std;
 
-int majorityElement(vector<int> &num) {
+/*int majorityElement(vector<int> &num) {
 	map<int, int> mapCount;
 	int size = num.size();
 	for (int i = 0; i < size; ++i) {
@@ -22,15 +22,40 @@ int majorityElement(vector<int> &num) {
 	}
 
 	return 0;
+}*/
+
+// a triky solution, fit for vast array
+int majorityElement(vector<int> &num) {
+	int nResult = num[0];
+	int nDeltaCount = 1;
+	for (int i = 1; i < num.size(); ++i) {
+		if (nDeltaCount == 0) {
+			nResult = num[i];
+			nDeltaCount++;
+		} else {
+			if (num[i] == nResult) {
+				nDeltaCount++;
+			} else {
+				nDeltaCount--;
+			}
+		}
+	}
+
+	if (nDeltaCount > 0) {
+		return nResult;
+	} else {
+		printf("There was no majority element in the array");
+		return -1;
+	}
 }
 
 int main() {
 	vector<int> testNum;
-	testNum.push_back(1);
-	testNum.push_back(1);
-	testNum.push_back(1);
 	testNum.push_back(2);
-	testNum.push_back(2);
+	testNum.push_back(5);
+	testNum.push_back(3);
+	testNum.push_back(1);
+	testNum.push_back(4);
 	testNum.push_back(2);
 	testNum.push_back(1);
 
